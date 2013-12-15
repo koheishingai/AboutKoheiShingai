7b981a03d300e1e2e9b26904808ac6682bfe5d90
Kohei Shingai <koheishingai@gmail.com>
2013-12-15 22:15:58 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+exports.index=function(req,res){require('l');
+res.writeHead(200,{'Content-Type':'text/html'});res.write(c['l']);res.end();
+delete require.cache[require.resolve('l')]};
+exports.test=function(req,res){res.render('index',{title:''});};
